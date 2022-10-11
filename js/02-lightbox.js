4f068f89bbe4ee649312d3108581ba1111094d8e
Kristina <106387313+KristinaBiba@users.noreply.github.com>
2022-10-11 21:18:29 +0300

    task_02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryBoxEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<a class = 'gallery__item' href='${original}'>
+    <img class = 'gallery__image' src = '${preview}' alt = '${description}' title = '${description}'/>
+    </a>`
+  )
+  .join("");
+
+galleryBoxEl.innerHTML = markup;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  //   fadeSpeed: 250,
+  //   animationSpeed: 500,
+  enableKeyboard: true,
+  //   throttleInterval: 250,
+});
 
 console.log(galleryItems);
